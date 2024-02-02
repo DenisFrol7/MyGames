4685707cfe3068f204dbc06f13f404e847438765
Denis <domino387@gmail.com>
2024-02-03 00:26:07 +0300

    first commit

---
+++ backend/myGame/src/main/java/com/example/myGame/MyGameApplication.java
@@ -0,0 +1,11 @@
+package com.example.myGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameApplication.class, args);
+	}
+}
---
+++ backend/myGame/src/test/java/com/example/myGame/MyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
